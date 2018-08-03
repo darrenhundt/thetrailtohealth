7b082ebd21bf19ed1d78c58f91ce5cbe0736a3f9
Darren Hundt <darrenhundt@gmail.com>
2018-08-03 12:32:55 +0000

    Upload: scripts/hundt.js

--- scripts/hundt.js
+++ scripts/hundt.js
@@ -1,16 +1,18 @@
 // JavaScript Document
 $(document).ready(function(){
-	$( "#toggleCommentsLink" ).click(function() {
-	  $( "#comments-section" ).slideToggle( "slow", function() {
-		if ($("#toggleCommentsLink").html() == 'Open Comments') {
-			$("#toggleCommentsLink").html('Close Comments');
-		}
-		else {
-			$("#toggleCommentsLink").html('Open Comments');
-		}
-	  });
-	});
-	var socialWrapper = $('.ss-social-list-wrapper').first();
-	var mainImage = $('.entry-content .image-block').first();
-	mainImage.addClass('main-image-block');
+	if ($('body').hasClass('collection-type-blog')) {
+		$( "#toggleCommentsLink" ).click(function() {
+		  $( "#comments-section" ).slideToggle( "slow", function() {
+			if ($("#toggleCommentsLink").html() == 'Open Comments') {
+				$("#toggleCommentsLink").html('Close Comments');
+			}
+			else {
+				$("#toggleCommentsLink").html('Open Comments');
+			}
+		  });
+		});
+		var socialWrapper = $('.ss-social-list-wrapper').first();
+		var mainImage = $('.entry-content .image-block').first();
+		mainImage.addClass('main-image-block');
+	}
 });
