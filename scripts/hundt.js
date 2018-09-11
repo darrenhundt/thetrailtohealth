9050387a7adf749aeca59ae9249014ea5d5e5d06
Darren Hundt <darrenhundt@gmail.com>
2018-09-11 04:56:15 +0000

    Upload: scripts/hundt.js

--- scripts/hundt.js
+++ scripts/hundt.js
@@ -29,28 +29,7 @@ $(document).ready(function(){
 
 	$('.amazon-block .productDetails .product-title').matchHeight();
 	
-	
-	$('.category-cookbook').each(function(i){
-		var imgURL = $(this).find('.popup-top img').first().attr('src');
-		$(this).find('.popup-top').css('background-image','url("' + imgURL + '")');
-		var newTriggerClass= 'modal-launcher-' + i;
-		var newTargetClass = 'recipe-modal-' + i;
-		$(this).find('.content-wrapper').addClass(newTriggerClass);
-		$(this).find('.popup-content').addClass(newTargetClass);
 
-	});
-	
-	$(function() {
-		$('.modal-launcher-0').on('click', function( e ) {
-				e.preventDefault();
-				Custombox.open({
-						target: '.recipe-modal-0',
-						effect: 'fadein',
-					overlay:false
-				});
-				e.preventDefault();
-		});
-  });
 });
 
 
