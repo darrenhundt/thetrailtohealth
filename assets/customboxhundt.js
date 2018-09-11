a9b88acd56940ad1344185c7b0dc8450415bb482
Darren Hundt <darrenhundt@gmail.com>
2018-09-11 04:59:15 +0000

    Upload: assets/customboxhundt.js

--- assets/customboxhundt.js
+++ assets/customboxhundt.js
@@ -19,11 +19,11 @@ $(document).ready(function(){
 	$(function() {
 		$('.modal-launcher-0').on('click', function( e ) {
 				e.preventDefault();
-				Custombox.open({
+				Custombox.modal({
 						target: '.recipe-modal-0',
 						effect: 'fadein',
 					overlay:false
-				});
+				}).open();;
 				e.preventDefault();
 		});
   });
@@ -32,3 +32,20 @@ $(document).ready(function(){
 	
 	
 });
+
+
+//  $(document).on('click', '#open-multiple', function() {
+//    new Custombox.modal({
+//      content: {
+//        effect: effects[Math.floor(Math.random() * effects.length)],
+//        target: '#demo-modal-multiple-1',
+//        animateFrom: animate[Math.floor(Math.random() * animate.length)],
+//        animateTo: animate[Math.floor(Math.random() * animate.length)],
+//        positionX: positionX[Math.floor(Math.random() * positionX.length)],
+//        positionY: positionY[Math.floor(Math.random() * positionY.length)],
+//      },
+//      overlay: {
+//        active: Math.random() < 0.5
+//      }
+//    }).open();
+//  });
