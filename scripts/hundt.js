1849436a5f9a4b03e89f20d75fcd38830423f0bd
Darren Hundt <darrenhundt@gmail.com>
2020-04-10 20:03:45 +0000

    Upload: scripts/hundt.js

--- scripts/hundt.js
+++ scripts/hundt.js
@@ -131,7 +131,7 @@ $(document).ready(function(){
 	
 	if ($('#collection-5e84a602cb44b3644a90a8a1').length) {
 		var contentHeader = $('#page-body-header');
-		var filterContainer = contentHeader.find('.html-block p');
+		var filterContainer = contentHeader.find('.html-block h3');
 		filterContainer.html('<a href="#all" class="brand-filter-link brand-filter-link-all">All</a>&nbsp;|&nbsp;');
 		$('.main-content h2').each(function(){
 			var tagLabel = $(this).html().toLowerCase();
