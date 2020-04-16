8ef5ae49482c9e1ab829ec1141052bff323a58ea
Darren Hundt <darrenhundt@gmail.com>
2020-04-16 15:49:10 +0000

    Upload: scripts/hundt.js

--- scripts/hundt.js
+++ scripts/hundt.js
@@ -138,7 +138,7 @@ $(document).ready(function(){
 			var thisEl = $(this);
 			var summaryJsonString = nextEl.attr('data-block-json');
 			var summaryJson = jQuery.parseJSON(summaryJsonString);
-			console.log(summaryJson.filter);
+			console.log(summaryJson.filter.category);
 			thisEl.add(nextEl).add(nextNextEl).wrapAll('<div class="category-wrapper" />');
 		});
 	}
