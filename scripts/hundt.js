8f8827c703ccff2ed85d275fe416e32c095cc3cd
Darren Hundt <darrenhundt@gmail.com>
2020-07-26 18:03:24 +0000

    Upload: scripts/hundt.js

--- scripts/hundt.js
+++ scripts/hundt.js
@@ -155,7 +155,7 @@ $(document).ready(function(){
 	});
 
 	$('.amazon-block .productDetails .product-title').matchHeight();
-	$('.video-recipe-item h2').matchHeight();
+	$('.recipe-title-length-wrapper').matchHeight();
 	
 	$('a[href*="amazon"]').each(function() {
 		$(this).addClass('amazon-link');
