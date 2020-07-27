ab4ddd1412712ccad35546fe51137d3ebb2d3669
Darren Hundt <darrenhundt@gmail.com>
2020-07-27 17:37:42 +0000

    Upload: scripts/sandbox.js

--- scripts/sandbox.js
+++ scripts/sandbox.js
@@ -1,12 +1,6 @@
 // JavaScript Document
 $(document).ready(function(){
 	console.log('hello from sandbox');
-	$.ajax({
-		method: "POST",
-		url: "some.php",
-		dataType: "json",
-		data: {}
-	}).done(json => console.log(json));
 	
 	$.getJSON( "/recipes?format=json", function( data ) {
 		$.each( data, function( key, val ) {
