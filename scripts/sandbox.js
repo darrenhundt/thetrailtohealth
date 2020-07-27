1effb96770d38abab3d6e0f915dcc7412929e2c9
Darren Hundt <darrenhundt@gmail.com>
2020-07-27 17:37:07 +0000

    Upload: scripts/sandbox.js

--- scripts/sandbox.js
+++ scripts/sandbox.js
@@ -1,4 +1,17 @@
 // JavaScript Document
 $(document).ready(function(){
 	console.log('hello from sandbox');
+	$.ajax({
+		method: "POST",
+		url: "some.php",
+		dataType: "json",
+		data: {}
+	}).done(json => console.log(json));
+	
+	$.getJSON( "/recipes?format=json", function( data ) {
+		$.each( data, function( key, val ) {
+			console.log(key)
+			/* items.push( "<li id='" + key + "'>" + val + "</li>" ); */
+		});
+	});
 });
