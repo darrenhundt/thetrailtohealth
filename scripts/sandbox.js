338475a28a5c90b2a7f7de08eb7be02512ae1865
Darren Hundt <darrenhundt@gmail.com>
2020-07-27 17:59:32 +0000

    Upload: scripts/sandbox.js

--- scripts/sandbox.js
+++ scripts/sandbox.js
@@ -17,7 +17,7 @@ $(document).ready(function(){
 	
 	$.getJSON( newUrl, function( data ) {
 		$.each( data.items, function( key, val ) {
-			console.log(val.addedOn)
+			console.log(val.addedOn);
 			offsetDynamic = val.addedOn;
 			/* items.push( "<li id='" + key + "'>" + val + "</li>" ); */
 		});
