9965048b7963c28b91deb37c5465a2d739d70bb1
Darren Hundt <darrenhundt@gmail.com>
2020-07-27 18:09:22 +0000

    Upload: scripts/sandbox.js

--- scripts/sandbox.js
+++ scripts/sandbox.js
@@ -1,26 +1,48 @@
 // JavaScript Document
 $(document).ready(function(){
 	console.log('hello from sandbox');
-	var offsetDynamic;
 	
-	$.getJSON( "/recipes?format=json", function( data ) {
+	var firstRecipeUrl = "/recipes?format=json";
+	var currentRecipeUrl = firstRecipeUrl;
+	var fetchMore = true;
+	
+	$.ajax({
+		url: currentRecipeUrl,
+		dataType: 'json',
+		async: false,
+		success: function(data) {
+			//console.log(data.items);
+			$.each( data.items, function( key, val ) {
+				console.log(val.title);
+				/* items.push( "<li id='" + key + "'>" + val + "</li>" ); */
+			});
+			currentRecipeUrl = data.pagination.nextPageUrl;
+		}
+	});
+	
+	$.ajax({
+		url: currentRecipeUrl,
+		dataType: 'json',
+		async: false,
+		success: function(data) {
+			//console.log(data.items);
+			$.each( data.items, function( key, val ) {
+				console.log(val.title);
+				/* items.push( "<li id='" + key + "'>" + val + "</li>" ); */
+			});
+		}
+	});
+	
+	/*
+	$.getJSON( firstRecipeUrl, function( data ) {
 		if (data.pagination.nextPage) {
 			console.log(data.pagination.nextPageUrl);
 		}
 		$.each( data.items, function( key, val ) {
 			console.log(val.addedOn);
-			/* items.push( "<li id='" + key + "'>" + val + "</li>" ); */
-		});
-	});
-	
-	//var newUrl = "/recipes?format=json&offset=" + offsetDynamic;
-	
-	//console.log(newUrl);
-	/*
-	$.getJSON( newUrl, function( data ) {
-		$.each( data.items, function( key, val ) {
-			console.log(val.addedOn);
 		});
 	});
 	*/
+	
+	
 });
