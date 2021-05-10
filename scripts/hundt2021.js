6acdfa4aaeb5eaf1f7440876316bd18812cf7526
chris brody <chris@hope-os.com>
2021-05-10 12:33:53 +0000

    Upload: scripts/hundt2021.js

--- scripts/hundt2021.js
+++ scripts/hundt2021.js
@@ -63,16 +63,7 @@ $(document).ready(function(){
     cellAlign: 'center',
     pauseAutoPlayOnHover: true
   });
-  $('a').click(function(e) {
 
-    var target = $(e.target);
-
-    console.log(target);
-
-    if($(target).is(':contains("Coaching")')){
-      console.log("Coaching")
-    }
-  });​
 
   /*
   $('.custom-nav-list').slick({
