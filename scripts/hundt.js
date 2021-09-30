ab40e003a38f3ce585380029ea4a6ea9defa86b9
David Ayers <davidayers1125@gmail.com>
2021-09-30 20:16:17 +0000

    Upload: scripts/hundt.js

--- scripts/hundt.js
+++ scripts/hundt.js
@@ -200,10 +200,10 @@ $(document).ready(function(){
 			$(this).attr('href','/shop-page/');
 			$(this).attr('onclick','');
 		}
-		/*if (linkContent == 'Coaching') {
+		if (linkContent == 'Coaching') {
 			$(this).attr('href','/coaching/');
 			$(this).attr('onclick','');
-		}*/
+		}
 		if (linkContent == 'Press') {
 			$(this).attr('href','/press/');
 			$(this).attr('onclick','');
